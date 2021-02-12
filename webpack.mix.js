24cd93b21b4296b9dec71200c7415c3dd5209d41
Anoskh <kimhab.zeecode@gmail.com>
2021-02-12 14:20:45 +0700

    VP64: Install and Config Lightbox2

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,11 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    /* .sass('resources/sass/app.scss', 'public/css') */;
+    .sass('resources/sass/app.scss', 'public/css');
 
 // More about Laravel Mix https://laravel.com/docs/7.x/mix
 // Customize Own CSS | Javascript Style and Different File CSS | JavaScript
 mix.js('resources/js/custom.js', 'public/js')
     .sass('resources/sass/custom.scss', 'public/css');
+
 
 
