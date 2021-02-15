ecd3b361569c2ac63881c24f997be392dca865f1
Anoskh <kimhab.zeecode@gmail.com>
2021-02-15 16:28:39 +0700

    VP80: Apply Auth() middleware to the rest of the controllers

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Str;
 
 class TagController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth')->except(['index']);
+    }
+
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,10 @@ use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth')->except(['show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
